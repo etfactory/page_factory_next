8214d6fb7326f67671ebff37b5638d7027b793f3
factory <factory@etfactory.dev>
2025-10-10 16:27:31 +0900

    Contact Me 수정

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -12,6 +12,7 @@ import "./styles/contact.css";
 export const ContactSection = () => {
     const form = useRef<HTMLFormElement>(null);
 
+    // 이메일 전송 함수
     const sendEmail = (e: React.FormEvent) => {
         e.preventDefault();
 
@@ -24,10 +25,15 @@ export const ContactSection = () => {
             )
             .then((result) => {
                 console.log(result.text);
+
+                // 메일 전송 되었다는 확인 메시지 출력
+                alert("메일이 성공적으로 전송되었습니다. 빠른 시일 내에 답변드리겠습니다!");
             })
             .catch((error) => {
                 console.log(error.text);
             });
+
+            form.current.reset(); // 폼 초기화
         }
     };
 
@@ -37,9 +43,9 @@ export const ContactSection = () => {
                 <h1 className="section-title">Contact Me</h1>
                 <form ref={form} onSubmit={sendEmail}>
                     <label>Name</label>
-                    <input type="text" name="user_name" required />
+                    <input type="text" name="name" required />
                     <label>Email</label>
-                    <input type="email" name="user_email" required />
+                    <input type="email" name="email" required />
                     <label>Message</label>
                     <textarea name="message" required />
                     <input type="submit" value="Send" />
